6e4430c5e482d41a263672fe23889b356721f88f
pkathail <pkathail@pkathail-mbp.dhcp.thefacebook.com>
2015-06-29 11:16:17 -0700

    Created initial xcode project

---
+++ CountOnMe/CountOnMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CountOnMe
+//
+//  Created by Pooja Kathail on 6/29/15.
+//  Copyright (c) 2015 Pooja Kathail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
